88062a37b0d53c1cd5426494ed7159f0c09eeb6e
SidOTU <sidharthsuresh07@gmail.com>
2023-11-30 13:36:25 -0500

    added game manager and ui manager

---
+++ Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreManager : MonoBehaviour
+{
+    public static ScoreManager instance;
+
+    private int score; 
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void incrementScore()
+    {
+        score++;
+    }
+}
